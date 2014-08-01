6b9dc93f86b58d1a1bff791cd4976e23da735b46
Oliver Monneke <codersquad@googlemail.com>
2014-08-01 22:23:40 +0200

    CU-61 CSS + JS Dateien herunterladen und includen

--- src/installer/Install.php
+++ src/installer/Install.php
@@ -123,8 +123,8 @@ $installer = new Install();
         <link href="../lib/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet">
         <link href="../lib/plugins/fontawesome/css/font-awesome.css" rel="stylesheet">
         <!--[if lt IE 9]>
-          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <script src="../lib/plugins/js/html5shiv.min.js"></script>
+        <script src="../lib/plugins/js/respond.min.js"></script>
         <![endif]-->
         <style>
             .breadcrumb>li+li:before {
