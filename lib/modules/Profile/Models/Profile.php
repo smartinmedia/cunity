ee250470543f3db84e3137c633f97b599b5a231f
Oliver Monneke <o.monneke@uhk-koeln.de>
2014-10-07 10:04:45 +0200

    code style

--- lib/modules/Profile/Models/Profile.php
+++ lib/modules/Profile/Models/Profile.php
@@ -117,7 +117,7 @@ class Profile
      */
     protected function render()
     {
-        $view = new \Cunity\Profile\View\Profile();
+        $view = new Profile();
         $view->assign('profile', $this->profileData);
         $view->setMetaData(["title" => $this->profileData['name']]);
         $view->render();
