1a91bb4b90cdd568aeadd90bfa6aedad280f6942
Oliver Monneke <codersquad@googlemail.com>
2015-01-13 08:57:36 +0100

    bugfix

--- lib/modules/Core/Module.php
+++ lib/modules/Core/Module.php
@@ -48,7 +48,7 @@ class Module
     /**
      * @var array
      */
-    private static $FIXMODULES = ['admin', 'start', 'register', 'notifications', 'comments'];
+    private static $FIXMODULES = ['admin', 'start', 'register', 'notifications', 'comments', 'search'];
 
     /**
      * @var
