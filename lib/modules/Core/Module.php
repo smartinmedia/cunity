89ce397c3dd0dc43ae228c51e455b5169e38b6ed
Oliver Monneke <codersquad@googlemail.com>
2015-01-14 05:16:40 +0100

    CU-212 Like / dislike of photo

--- lib/modules/Core/Module.php
+++ lib/modules/Core/Module.php
@@ -48,7 +48,7 @@ class Module
     /**
      * @var array
      */
-    private static $FIXMODULES = ['admin', 'start', 'register', 'notifications', 'comments', 'search'];
+    private static $FIXMODULES = ['admin', 'start', 'register', 'notifications', 'comments', 'search', 'likes'];
 
     /**
      * @var
